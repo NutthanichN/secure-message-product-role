5a8902181808c9685ae70d3522646f1aa1a505fe
Nutthanich Narphromar <monaprom134@gmail.com>
2021-03-24 06:47:50 +0700

    Add roles to customize web pages

---
+++ src/main/java/ku/message/service/UserService.java
@@ -0,0 +1,41 @@
+package ku.message.service;
+
+import ku.message.model.User;
+import ku.message.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean isUsernameAvailable(String username) {
+        return repository.findByUsername(username) == null;
+    }
+
+    public int createUser(User user) {
+        // newUser may differ from user somehow??
+        User newUser = new User();
+        newUser.setFirstname(user.getFirstname());
+        newUser.setLastname(user.getLastname());
+        newUser.setUsername(user.getUsername());
+        newUser.setRole(user.getRole());
+
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+
+        newUser.setPassword(hashedPassword);
+
+        repository.save(newUser);
+        return 1;
+    }
+
+    public User getUser(String username) {
+        return repository.findByUsername(username);
+    }
+}
