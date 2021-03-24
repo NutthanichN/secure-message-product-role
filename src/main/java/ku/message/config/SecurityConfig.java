d4a582cca686bdb9e7a78bad22d772f1b856b307
Nutthanich Narphromar <monaprom134@gmail.com>
2021-03-24 07:00:21 +0700

    Add role to limit access to web page

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -25,7 +25,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/home", "/signup", "/css/**", "/js/**").permitAll()
+                .antMatchers("/home", "/signup", "/css/**", "/js/**")
+                .permitAll()
+                .antMatchers("/product/add").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/product", "/message", "/post")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
                 .anyRequest().authenticated();
 
         http.formLogin()
